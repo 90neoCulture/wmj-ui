e88be4b551c4fd15a6ad9bb340bad8c0204efa55
90新技术 <wangmeijuan@touchealth.com>
2020-09-14 20:56:52 +0800

    feat:cell

--- src/components/cell/index.jsx
+++ src/components/cell/index.jsx
@@ -1,7 +1,57 @@
 const WmjCell = {
   functional: true,
   props: {
-
+    title: [Number, String],
+    value: [Number, String],
+    label: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    iconPrefix: {
+      type: String,
+      default: 'wmj-icon'
+    },
+    to: [String, Object],
+    url: {
+      type: String,
+      default: ''
+    },
+    border: {
+      type: Boolean,
+      default: true
+    },
+    replace: {
+      type: Boolean,
+      default: false
+    },
+    clickable: {
+      type: Boolean,
+      default: false
+    },
+    isLink	: {
+      type: Boolean,
+      default: false
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    center: {
+      type: Boolean,
+      default: false
+    },
+    arrowDirection: {
+      type: String,
+      default: 'right'
+    }
   },
   render: (h, context) => {
     console.log(h, context)
