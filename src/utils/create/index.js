664952eb2f5cdf076360e86872b55fd20e889d8d
90新技术 <wangmeijuan@touchealth.com>
2020-09-28 22:04:17 +0800

    fix:myBem语法使用错误

--- src/utils/create/index.js
+++ src/utils/create/index.js
@@ -1,8 +1,9 @@
 import { createComponent } from './component'
-import { myBem } from '../myBem'
+import myBem from '../myBem'
 
 export function createNamespace(name) {
   name = `wmj-${name}`
+  myBem(name)
   console.log(typeof createComponent(name))
   return [createComponent(name), myBem(name)]
 }
